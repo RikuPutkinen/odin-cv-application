0c15dc0ca5d2500e560217a59bc3a9a2dd56cada
Riku Putkinen <putkinenriku@gmail.com>
2023-03-06 18:33:53 +0200

    Add functionality to add, edit, and remove education

--- src/App.js
+++ src/App.js
@@ -6,15 +6,107 @@ import General from './components/General';
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.addEducationField = this.addEducationField.bind(this);
+    this.removeEducationField = this.removeEducationField.bind(this);
+    this.handleEducationChange = this.handleEducationChange.bind(this);
+
+    this.state = {
+      general: {
+        name: '',
+        email: '',
+        phone: ''
+      },
+      education: []
+      /*{
+        school: '',
+        title: '',
+        date: ''
+      }*/,
+      experience: []
+      /*{
+        company: '',
+        title: '',
+        task: '',
+        start: '',
+        end: ''
+      }*/
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const formJSON = Object.fromEntries(formData.entries());
+    console.log(formJSON);
+    
+    this.setState({
+      general: {
+        name: formJSON.name,
+        email: formJSON.email,
+        phone: formJSON.phone
+      },
+      education: []
+      /*{
+        id: ''
+        school: '',
+        title: '',
+        date: ''
+      }*/,
+      experience: []
+      /*{
+        id: ''
+        company: '',
+        title: '',
+        task: '',
+        start: '',
+        end: ''
+      }*/
+    });
+  };
+
+  addEducationField() {
+    this.setState(
+      {
+        education: [...this.state.education, {
+          id: crypto.randomUUID(),
+          school: '',
+          title: '',
+          date: ''}]}
+    )
+  }
+
+  removeEducationField(id) {
+    this.setState({education: this.state.education.filter(item => {
+      return item.id !== id;
+    })})
+  }
+
+  handleEducationChange(id, e) {
+    const data = this.state.education.map(item => {
+      if (item.id !== id) return item;
+      else {
+        item[e.target.name] = e.target.value;
+        return item;
+      }
+    })
+    this.setState({education: data});
   }
 
   render() {
     return (
-      <div className="App">
-        <General />
-        <Education />
-        <Experience />
-      </div>
+      <form className="App" onSubmit={this.handleSubmit}>
+        <General data={this.state.general}/>
+        <Education
+          data={this.state.education}
+          onAdd={this.addEducationField}
+          onRemove={this.removeEducationField}
+          onChange={this.handleEducationChange}/>
+        <Experience
+          data={this.state.experience}/>
+        <button type='submit'>Submit</button>
+      </form>
     );
   };
 }
--- src/components/Education.js
+++ src/components/Education.js
@@ -7,20 +7,18 @@ class EducationField extends Component {
 
   render() {
     return (
-      <div className="EducationField">
-        <fieldset>
-          <label htmlFor="school">School name</label>
-          <input name="school" ></input>
+      <fieldset>
+        <label htmlFor="school">School name</label>
+        <input name="school" ></input>
 
-          <label htmlFor="title">Title of study</label>
-          <input name="title" ></input>
+        <label htmlFor="title">Title of study</label>
+        <input name="title" ></input>
 
-          <label type="date" htmlFor="date">Date of study</label>
-          <input type="date" name="date" ></input>
+        <label type="date" htmlFor="date">Date of study</label>
+        <input type="date" name="date" ></input>
 
-          <button type='button'>Remove Education</button>
-        </fieldset>
-      </div>
+        <button type='button'>Remove Education</button>
+      </fieldset>
     );
   };
 }
@@ -34,8 +32,37 @@ class Education extends Component {
     return (
       <fieldset className="Education">
         <legend>Educational Experience</legend>
-        <EducationField />
-        <button type='button'>Add New Education</button>
+        {this.props.data.map((item) => {
+          const {id, school, title, date} = item;
+          return (
+            <fieldset key={id}>
+              <label htmlFor="school">School name</label>
+              <input
+                name="school" 
+                value={school}
+                onChange={e => this.props.onChange(id, e)}
+                ></input>
+
+              <label htmlFor="title">Title of study</label>
+              <input
+                name="title"
+                value={title}
+                onChange={e => this.props.onChange(id, e)}
+                ></input>
+
+              <label type="date" htmlFor="date">Date of study</label>
+              <input
+                type="date"
+                name="date" 
+                value={date}
+                onChange={e => this.props.onChange(id, e)}
+                ></input>
+
+              <button type='button' onClick={(e) => this.props.onRemove(id, e)}>Remove Education</button>
+            </fieldset>
+          )
+        })}
+        <button type='button' onClick={this.props.onAdd}>Add New Education</button>
       </fieldset>
     );
   };
